079281bdfb5460370ff4a839d40f32ca14f594c8
JesseSoeteman <67485119+JesseSoeteman@users.noreply.github.com>
2023-01-19 10:29:18 +0100

    Moved the classes to their own file.

---
+++ src/Statics/Pattern.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace ValidateGetPostInput\Statics;
+
+abstract class Pattern
+{
+    const NO_PATTERN = 0;
+    const VALIDATE_EMAIL = 1;
+    const REGEX = 2;
+}
--- src/Statics/RequestType.php
+++ src/Statics/RequestType.php
@@ -7,10 +7,3 @@ abstract class RequestType
     const GET = 0;
     const POST = 1;
 }
-
-abstract class Pattern
-{
-    const NO_PATTERN = 0;
-    const VALIDATE_EMAIL = 1;
-    const REGEX = 2;
-}

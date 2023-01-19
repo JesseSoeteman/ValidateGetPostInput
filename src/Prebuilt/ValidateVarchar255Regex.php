0e2da0f6315d9eb3132934643f234d62ce0000d0
JesseSoeteman <67485119+JesseSoeteman@users.noreply.github.com>
2023-01-19 21:11:28 +0100

    Create and change prebuilt classes

--- src/Prebuilt/ValidateVarchar255Regex.php
+++ src/Prebuilt/ValidateVarchar255Regex.php
@@ -3,30 +3,34 @@
 namespace ValidateGetPostInput\Prebuilt;
 
 use ValidateGetPostInput\Statics\RequestType;
+use ValidateGetPostInput\Statics\Pattern;
 use ValidateGetPostInput\Statics\DataType;
 
 use ValidateGetPostInput\ValidateGetPostInput;
 use ValidateGetPostInput\Classes\ValidateInputSettings;
 
 /**
- * ValidateText class to set the settings for the validation.
- * This class is used to validate text.
+ * ValidateVarchar255Regex class to set the settings for the validation.
+ * This class is used to validate a varchar(255) field, with a regex pattern.
  * 
+ * - This field is required. (this can be changed)
  * - The text must be a string.
  * - The text must be at least 1 character long.
- * - The text can be at most 65535 characters long.
+ * - The text can be at most 255 characters long. (this can be changed)
  *
  * @author  Jesse Soeteman
  * @version 1.0
  * @since   2023-01-18
  */
-class ValidateText extends ValidateGetPostInput
+class ValidateVarchar255Regex extends ValidateGetPostInput
 {
-    public function __construct($key, $request_type = RequestType::GET, $required = true, $max_lenght = 65535)
+    public function __construct($key, $reqex, $request_type = RequestType::GET, $required = true, $max_lenght = 255)
     {
         $settings = new ValidateInputSettings();
         $settings->input_type = $request_type;
         $settings->required = $required;
+        $settings->pattern = Pattern::REGEX;
+        $settings->regex_pattern = $reqex;
         $settings->data_type = DataType::STRING;
         $settings->check_min_max = true;
         $settings->min = 1;

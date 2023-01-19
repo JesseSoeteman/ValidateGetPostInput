0977d1ea5c2b69e407610c5d51025dba53fd0b1e
JesseSoeteman <67485119+JesseSoeteman@users.noreply.github.com>
2023-01-19 21:56:54 +0100

    more bugs, and forgot to add description

--- src/Prebuilt/ValidateJSON.php
+++ src/Prebuilt/ValidateJSON.php
@@ -8,6 +8,18 @@ use ValidateGetPostInput\Statics\DataType;
 use ValidateGetPostInput\Classes\ValidateInputSettings;
 use ValidateGetPostInput\ValidateGetPostInput;
 
+/**
+ * ValidateJSON class to set the settings for the validation.
+ * This class is used to validate a JSON object.
+ * 
+ * - The request type is GET. (this can be changed to POST)
+ * - The value is required. (this can be changed)
+ * - The value must be a JSON object.
+ *
+ * @author  Jesse Soeteman
+ * @version 1.0
+ * @since   2023-01-19
+ */
 class ValidateJSON extends ValidateGetPostInput
 {
     public function __construct($key, $request_type = RequestType::GET, $required = true)
